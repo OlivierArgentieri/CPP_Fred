38af9f395c0f03f9092fa3d03b35821095ac0def
Argentieri <olivierargentieri@gmail.com>
2020-02-17 17:16:06 +0100

    continue vec3 unit

--- MO3D/mO3D/mO3D/TestUnit.cpp
+++ MO3D/mO3D/mO3D/TestUnit.cpp
@@ -12,6 +12,7 @@ using namespace mo3d;
 #define ASSERT_EPSILON(givenVal, approxVal) assert((givenVal - approxVal) < EPSILON && (givenVal - approxVal) > -EPSILON)
 
 
+#pragma region vec2
 void UnitTest::TestVec2(float _x = 0, float _y = 1)
 {
 	
@@ -225,46 +226,234 @@ void UnitTest::TestVec2InferiorEquals()
 	assert(_v2 == vec2(2, 4));
 	assert(_v1 <= _v2);
 }
+#pragma endregion
 
-
+#pragma region vec3
 void UnitTest::TestVec3(float _x = 0, float _y = 0, float _z = 0)
 {
-	vec3 _vec;
-	assert(_vec.x == 0);
-	assert(_vec.y == 0);
-	assert(_vec.z == 0);
+	TestVec3Constructor(_x, _y, _z);
+	TestVec3Epsilon();
+	TestVec3Normalize();
+	TestVec3Distance();
+		   
+	TestVec3Dot();
+		   
+	TestVec3Mul();
+	TestVec3Div();
+	TestVec3Addition();
+	TestVec3Subtraction();
+		   
+	TestVec3Equals();
+	TestVec3NotEquals();
+	TestVec3Superior();
+	TestVec3SuperiorEquals();
+	TestVec3Inferior();
+	TestVec3InferiorEquals();
+}
 
-	 vec3 _vec1 = vec3(_x, _y, _z);
-	assert(_vec1.x == _x);
-	assert(_vec1.y == _y);
-	assert(_vec1.z == _z);
 
+void UnitTest::TestVec3Constructor(float _x = 0, float _y = 1, float _z = -1)
+{
+	vec3 _vec1;
+	assert(_vec1.x == 0);
+	assert(_vec1.y == 0);
+	assert(_vec1.z == 0);
 
-	// test epsilon
-	_vec = vec3(0.1234567f, 0.1234567f, 0.1234567f);
-	ASSERT_EPSILON(_vec.x, .1234567f);
-	ASSERT_EPSILON(_vec.y, .1234567f);
-	ASSERT_EPSILON(_vec.z, .1234567f);
+	vec3 _vec2 = vec3(_x, _y, _z);
+	assert(_vec2.x == _x);
+	assert(_vec2.y == _y);
+	assert(_vec2.z == _z);
+}
 
+void UnitTest::TestVec3Epsilon()
+{
+	vec3 _vec1 = vec3(0.1234567f, 0.1234567f, 0.1234567f);
+	ASSERT_EPSILON(_vec1.x, .1234567f);
+	ASSERT_EPSILON(_vec1.y, .1234567f);
+	ASSERT_EPSILON(_vec1.z, .1234567f);
+}
 
-	// test Magnitude : normalize
+void UnitTest::TestVec3Magnitude()
+{
 	vec3 _v1 = vec3(0, 0, 0);
 	vec3 _v2 = vec3(-1, 0, 0);
 
 	assert(_v1.Magnitude() == 0);
-	assert(_v1.Magnitude() == 0);
 	assert(_v2.Magnitude() == 1);
 	assert(_v1.Magnitude() == vec3::Magnitude(_v1));
 	assert(_v2.Magnitude() == vec3::Magnitude(_v2));
-	assert(_v2.Normalize() == vec3(-1,0,0));
-	assert(_v2.Normalize() != _v1);
+}
 
-	// + / +=
+void UnitTest::TestVec3Normalize()
+{
+	vec3 _v1 = vec3(0, 0, 0);
+	vec3 _v2 = vec3(-1, -1, -1);
+	vec3 _v3 = vec3(2, 2, 2);
+
+	assert(_v1.Normalize().Magnitude() == 0);
+	ASSERT_EPSILON(_v3.Normalize().Magnitude(), 1);
+	ASSERT_EPSILON(_v2.Normalize().Magnitude(), 1);
+}
+
+void UnitTest::TestVec3Distance()
+{
+	vec3 _v1 = vec3(0, 0, 0);
+	vec3 _v2 = vec3(-1, 0, 0);
+	vec3 _v3 = vec3(2, 0, 0);
+
+	assert(_v1.Distance(_v2) == 1);
+	assert(_v1.Distance(_v3) == 2);
+	assert(_v1.Distance(_v1) == 0);
+}
+
+
+void UnitTest::TestVec3Cross()
+{
+	vec3 _v1 = vec3(1, 1, 1);
+	vec3 _v2 = vec3(1, 1, 1);
+
+	assert(_v1.Cross(_v2) == 0);
+}
+
+
+void UnitTest::TestVec3Dot()
+{
+	vec3 _v1 = vec3(1, 0,0);
+	vec3 _v2 = vec3(0, 1, 0);
+
+	vec3 _v3 = vec3(1, 2, 3);
+	vec3 _v4 = vec3(1, 4, 5);
+
+	assert(_v1.Dot(_v2) == 0);
+	assert(_v3.Dot(_v4) == 24);
+}
+
+void UnitTest::TestVec3Mul()
+{
+	vec3 _v1 = vec3(2, 3, 4); 
+	vec3 _v2 = vec3(5, 5, 5);
+	_v1 * _v2;
+
+	assert(_v1 == vec3(2, 3, 4));
+	assert(_v2 == vec3(5, 5, 5));
+	assert(_v1 * _v2 == vec3(10, 15, 20));
+
+	_v1 *= _v2;
+	assert(_v1 == vec3(10, 15, 20));
+}
+
+void UnitTest::TestVec3Div()
+{
+	vec3 _v1 = vec3(2, 3, 4);
+	vec3 _v2 = vec3(5, 5, 5);
+	_v1 / _v2;
+
+	assert(_v1 == vec3(2, 3, 4));
+	assert(_v2 == vec3(5, 5, 5));
+	assert((_v1 / _v2) == vec3(0.4f, 0.6f, 0.8f));
+
+	_v1 /= _v2;
+	assert(_v1 == vec3(0.4f, 0.6f, 0.8f));
+
+}
+
+void UnitTest::TestVec3Addition()
+{
+	vec3 _v1 = vec3(2, 3, 4);
+	vec3 _v2 = vec3(5, 5, 5);
 	_v1 + _v2;
-	assert(_v1 == vec3(0, 0, 0));
+
+	assert(_v1 == vec3(2, 3, 4));
+	assert(_v2 == vec3(5, 5, 5));
+	assert((_v1 + _v2) == vec3(7, 8, 9));
+
 	_v1 += _v2;
+	assert(_v1 == vec3(7, 8, 9));
+}
+
+void UnitTest::TestVec3Subtraction()
+{
+	vec3 _v1 = vec3(2, 3, 4);
+	vec3 _v2 = vec3(5, 5, 5);
+	_v1 - _v2;
+
+	assert(_v1 == vec3(2, 3, 4));
+	assert(_v2 == vec3(5, 5, 5));
+	assert((_v1 - _v2) == vec3(-3, -2, -1));
+
+	_v1 -= _v2;
+	assert(_v1 == vec3(-3, -2, -1));
+}
+
+void UnitTest::TestVec3Equals()
+{
+	vec3 _v1 = vec3(2, 3, 4);
+	vec3 _v2 = vec3(2, 3, 4);
+	_v1 == _v2;
+
+	assert(_v1 == vec3(2, 3, 4));
+	assert(_v2 == vec3(2, 3, 4));
 	assert(_v1 == _v2);
 }
+
+void UnitTest::TestVec3NotEquals()
+{
+	vec3 _v1 = vec3(2, 3, 4);
+	vec3 _v2 = vec3(2, 4, 5);
+	_v1 != _v2;
+
+	assert(_v1 == vec3(2, 3, 4));
+	assert(_v2 == vec3(2, 4, 5));
+	assert(_v1 != _v2);
+}
+
+void UnitTest::TestVec3Superior()
+{
+	vec2 _v1 = vec2(2, 7);
+	vec2 _v2 = vec2(2, 4);
+	_v1 > _v2;
+
+	assert(_v1 == vec2(2, 7));
+	assert(_v2 == vec2(2, 4));
+	assert(_v1 > _v2);
+}
+
+void UnitTest::TestVec3SuperiorEquals()
+{
+	vec2 _v1 = vec2(2, 7);
+	vec2 _v2 = vec2(2, 4);
+	vec2 _v3 = vec2(2, 4);
+	_v1 > _v2;
+
+	assert(_v1 == vec2(2, 7));
+	assert(_v2 == vec2(2, 4));
+	assert(_v1 >= _v2);
+	assert(_v1 >= _v3);
+}
+
+void UnitTest::TestVec3Inferior()
+{
+	vec2 _v1 = vec2(2, 4);
+	vec2 _v2 = vec2(2, 7);
+	_v1 < _v2;
+
+	assert(_v1 == vec2(2, 4));
+	assert(_v2 == vec2(2, 7));
+	assert(_v1 < _v2);
+}
+
+void UnitTest::TestVec3InferiorEquals()
+{
+	vec2 _v1 = vec2(2, 4);
+	vec2 _v2 = vec2(2, 4);
+	_v1 <= _v2;
+
+	assert(_v1 == vec2(2, 4));
+	assert(_v2 == vec2(2, 4));
+	assert(_v1 <= _v2);
+}
+
+#pragma endregion 
 
 void UnitTest::TestVec4(float _x = 0, float _y = 0, float _z = 0, float _w = 0)
 {
--- MO3D/mO3D/mO3D/UnitTest.h
+++ MO3D/mO3D/mO3D/UnitTest.h
@@ -23,6 +23,26 @@ private:
 
 
 	static void TestVec3(float _x, float _y, float _z);
+	static void TestVec3Constructor(float _x, float _y, float _z);
+	static void TestVec3Epsilon();
+	static void TestVec3Magnitude();
+	static void TestVec3Normalize();
+	static void TestVec3Distance();
+	static void TestVec3Cross();
+	static void TestVec3Dot();
+	static void TestVec3Mul();
+	static void TestVec3Div();
+	static void TestVec3Addition();
+	static void TestVec3Subtraction();
+	static void TestVec3Equals();
+	static void TestVec3NotEquals();
+	static void TestVec3Superior();
+	static void TestVec3SuperiorEquals();
+	static void TestVec3Inferior();
+	static void TestVec3InferiorEquals();
+
+
+
 	static void TestVec4(float _x, float _y, float _z, float _w);
 	static void TestMat4();
 	UnitTest();

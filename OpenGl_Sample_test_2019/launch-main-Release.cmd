d90a0aa74bdce32e8e2dbb7fe1d890f93c4e1f21
Argentieri <olivierargentieri@gmail.com>
2020-02-22 17:10:32 +0100

    TEST 2019

---
+++ OpenGl_Sample_test_2019/launch-main-Release.cmd
@@ -0,0 +1,9 @@
+cd D:/ArgentieriOlivier/PullGithub/CPP_Fred/OpenGl_Sample/main/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"D:\ArgentieriOlivier\PullGithub\CPP_Fred\OpenGl_Sample_test_2019\Release\main.exe"  
+pause
